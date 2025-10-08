ad923bfe72d9b3baebfe3ef66cf95de45350e24e
LeeS89 <leeslammon@outlook.com>
2025-10-08 02:22:30 +0100

    UpdateTest

--- ProjectRemaster/Source/ProjectRemaster/Private/Interfaces/DeflectableInterface.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Interfaces/DeflectableInterface.h"
-
-// Add default functionality here for any IDeflectableInterface functions that are not pure virtual.
-
-
